024bfa5bfc65ba40b506fc66102f16413ce8be13
Lucky-Badger <luckybadger10@gmail.c>
2021-04-26 19:16:47 -0500

    tests

--- classDb/models.py
+++ classDb/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Course(models.Model):
@@ -8,6 +9,17 @@ class Course(models.Model):
     course_name = models.CharField
     is_lab = models.BooleanField
 
+
 class CourseAssignment(models.Model):
     course_id = models.ForeignKey
     user_id = models.ForeignKey
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    user_id = models.CharField(max_length=50)
+    number = models.CharField(max_length=50)
+    role = models.CharField(max_length=50)
+    assignment_ID = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
