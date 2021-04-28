4c94d7a0ca25fde6e320a13588d98377c69c9ffc
moltenpopsicle <mr.james.reggie@gmail.com>
2021-04-27 20:17:27 -0500

    fixed merge

--- classTracker/views.py
+++ classTracker/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from .models import Course
 
 class Home(View):
     def get(self, request):
@@ -7,11 +8,27 @@ class Home(View):
 
 class Courses(View):
     def get(self, request):
-        return render(request, "courses.html", {})
+        courses = Course.objects.all()
+        return render(request, "courses.html", {'courses': courses})
 
 class CreateCourse(View):
     def get(self, request):
         return render(request, "create_course.html", {})
+
+    def post(self, request):
+        course = Course()
+        course.course_name = request.POST.get('course_name')
+        course.course_no = request.POST.get('course_no')
+        course.section_no = request.POST.get('section_no')
+        course.is_lab = request.POST.get('is_lab') == "on"
+        print(course.course_no)
+        print(course.section_no)
+        print(course.course_name)
+        if course.course_no == "" or course.course_name == "" or course.section_no == "":
+            return render(request, "create_course.html", {"message": "ERROR: all fields must be filled out"})
+        else:
+            course.save()
+            return redirect("/courses")
 
 class create_User(View):
     def get(self, request):
