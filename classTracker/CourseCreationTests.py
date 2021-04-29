9dd3621442566b388b00637e3b4eeef154e8cab9
moltenpopsicle <mr.james.reggie@gmail.com>
2021-04-29 15:05:15 -0500

    made course creation look a bit better

--- classTracker/CourseCreationTests.py
+++ classTracker/CourseCreationTests.py
@@ -2,7 +2,7 @@ import unittest
 from django.test import TestCase, Client
 from django.conf import settings
 
-class CourseCreationTests(unittest.TestCase):
+class CourseCreationTests(TestCase):
     def setUp(self):
         self.client = Client()
 
